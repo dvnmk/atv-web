1b6dd4427f032ff6ca7bb679ac0a99f4ef8ce984
dvnmk <betelgeuse@gmail.com>
2026-07-18 06:29:01 +0100

    Add magic word example

---
+++ magic-word.example.lisp
@@ -0,0 +1,9 @@
+;; magic-word.example.lisp
+(in-package :atv-web)
+
+;; Replace this with your own magic word generator.
+;; This file is only an example.
+
+(defun today-magic-word ()
+  "Return today's access code."
+  "00000")
